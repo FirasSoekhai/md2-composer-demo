27e546f0072fa5a6c769a5b02c33d50d7190f4dd
Firas <firassoekhai@hotmail.nl>
2020-10-30 10:51:33 +0100

    yes

--- create_qrcode.php
+++ create_qrcode.php
@@ -1,14 +1,29 @@
 <?php
 // Bestand met functions inladen, deze regel laten staan aub
-require './includes/functions.php';
+// require './includes/functions.php';
 
 // Laad de composer autoloader in
-
+require '/      vendor/autoload.php';
 // Lees de documentatie van de bacon/bacon-qr-code package
 // Zo kom je te weten hoe je deze package kunt gebruiken
 // https://github.com/Bacon/BaconQrCode
 
 // Maak een QR code met als inhoud een leuke URL
+use CodeItNow\BarcodeBundle\Utils\QrCode;
+
+$qrCode = new QrCode();
+$qrCode
+    ->setText('https//:firassoekhai.com')
+    ->setSize(300)
+    ->setPadding(10)
+    ->setErrorCorrection('high')
+    ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
+    ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
+    ->setLabel('Broodje Ekmek')
+    ->setLabelFontSize(16)
+    ->setImageType(QrCode::IMAGE_TYPE_PNG)
+;
+$qrcode->save('qrcode.png');
 // Tip: Gebruik de SvgImageBackEnd()
 
 // Sla de QR code op als afbeelding: "qrcode.svg"
--- read_qrcode.php
+++ read_qrcode.php
@@ -1,6 +1,8 @@
 <?php
 // Laad de composer autoloader in
-
+require '/vendor/autoload.php';
+$qrcode = new QrReader('qrcode.png');
+$text = $qrcode->text(); //return decoded text from QR Code
 // Lees de documentatie van de khanamiryan/qrcode-detector-decoder package:
 // Zo weet je hoe je deze package kunt gebruiken
 // https://github.com/khanamiryan/php-qrcode-detector-decoder/
@@ -13,3 +15,4 @@
 // Je hebt NU dus een "qrcode.png" die je kunt inladen
 
 // Zet de tekst op het scherm die in de QR Code zit!
+echo "\nQR CODE = " . $text . "\n\n";
